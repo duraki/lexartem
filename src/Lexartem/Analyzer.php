ccc2f6fda6620a35b8e272e6132f06cbf47cbdee
Halis Duraki <duraki.halis@nsoft.ba>
2017-10-24 19:10:14 +0200

    [added] interface for analyzer

--- src/Lexartem/Analyzer.php
+++ src/Lexartem/Analyzer.php
@@ -2,6 +2,12 @@
 
 namespace Lexartem;
 
+/**
+ * An interface for Lexartem analyzer. Allow different topics and depends on 
+ * language only.
+ *
+ * @interface Analyzer
+ */
 interface Analyzer
 {
     
